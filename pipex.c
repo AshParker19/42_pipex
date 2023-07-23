e3affd8c974b7af7dbcb1fc0f737702afbe1e7e8
AshParker19 <ashparker4219@gmail.com>
2023-07-23 19:45:25 +0100

    restructured, now the commands are not executed from the parent process

--- pipex.c
+++ pipex.c
@@ -6,11 +6,21 @@
 /*   By: anshovah <anshovah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 14:45:47 by anshovah          #+#    #+#             */
-/*   Updated: 2023/07/21 17:56:30 by anshovah         ###   ########.fr       */
+/*   Updated: 2023/07/23 19:35:00 by anshovah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+void	ft_free_array(char **arr)
+{
+	int	i;
+
+	i = -1;
+	while (arr[++i])
+		free(arr[i]);
+	free (arr);	
+}
 
 int	ft_check_access(char *path)
 {
@@ -87,36 +97,29 @@ void	ft_first_command(char *cmd1, int infile_fd, char **path_dirs, int fd[])
 	close(fd[1]);
 	argv = ft_split(cmd1, NULL, 0, ' ');
 	cmd_path = ft_find_command(argv[0], path_dirs);
-	i = -1;
-	// while (path_dirs[++i])
-	// 	free (path_dirs);
-	// free (path_dirs);
-	close (infile_fd);	
+	ft_free_array(path_dirs);
 	execve(cmd_path, argv, NULL);
 	perror("execve");
 	exit(EXECVE_ERROR);
 }
 
-void	ft_second_command(char *cmd2, int outfile_fd, char **path_dirs, int fd[])
+void	ft_second_command(char *cmd2, int outfile_fd, char **path_dirs, int fd2[])
 {
 	char	**argv;
 	char	*cmd_path;
 	int		i;
 
-	close(fd[1]);
-	dup2(fd[0], STDIN_FILENO);
+	close(fd2[1]);
 	dup2(outfile_fd, STDOUT_FILENO);
 	close (outfile_fd);
-	close(fd[0]);
+	dup2(fd2[0], STDIN_FILENO);
+	close(fd2[0]);
 	argv = ft_split(cmd2, NULL, 0, ' ');
 	cmd_path = ft_find_command(argv[0], path_dirs);
-	i = -1;
-	while (path_dirs[++i])
-		free (path_dirs);
-	free (path_dirs);
-	close(outfile_fd);	
+	ft_free_array(path_dirs);
 	execve(cmd_path, argv, NULL);
 	perror("execve");
+	exit(EXECVE_ERROR);
 }
 
 int	main(int ac, char *av[], char *env[])
@@ -125,25 +128,20 @@ int	main(int ac, char *av[], char *env[])
 	int		infile_fd;
 	int		outfile_fd;
 	int		fd[2];
-	int		pid;
+	int		pid1;
+	int		pid2;
 	int		pipe_check;
+	int		pipe_check2;
 
 	if (ac != 5 || (!*av[0] || !*av[1] || !*av[2] || !*av[3] || !*av[4]))
 	{
 		write(2, "Wrong arguments!\n", 18);
 		exit (ARGUMENT_ERROR);
 	}
-	path_dirs = ft_get_path(env);
 	if (!ft_check_access(av[1]))
 	{
 		perror("access");
 		exit (ACCESS_ERROR);
-	}
-	infile_fd = open(av[1], O_RDONLY);
-	if (infile_fd < 0)
-	{
-		perror("open");
-		exit (OPEN_ERROR);
 	}
 	pipe_check = pipe(fd);
 	if (pipe_check < 0)
@@ -151,26 +149,34 @@ int	main(int ac, char *av[], char *env[])
 		perror("pipe");
 		exit (PIPE_ERROR);
 	}
-	pid = fork();
-	if (pid < 0)
+	path_dirs = ft_get_path(env);
+	pid1 = fork();
+	if (pid1 < 0)
 	{
 		perror("fork");
 		exit (FORK_ERROR);
 	}
-	if (pid == 0)
+	if (pid1 == 0)
+	{
+		infile_fd = open(av[1], O_RDONLY);
 		ft_first_command(av[2], infile_fd, path_dirs, fd);
-	else
+	}
+	pid2 = fork();
+	if (pid2 < 0)
+	{
+		perror("fork");
+		exit (FORK_ERROR);
+	}
+	if (pid2 == 0)
 	{
 		outfile_fd = open(av[4], O_RDWR | O_CREAT | O_TRUNC, 0666);
-		if (outfile_fd < 0)
-		{
-			perror("open");
-			exit (OPEN_ERROR);
-		}
-		wait(NULL);
 		ft_second_command(av[3], outfile_fd, path_dirs, fd);
-		close(outfile_fd);
 	}
+	close(fd[0]);
+	close(fd[1]);
+	ft_free_array(path_dirs);
+	wait(NULL);
+	wait(NULL);
 }
 
 // check if any of the arguments are not empty strings or if the commands are correct
